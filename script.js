af64c6ec5a6725e1285d5dd4210d5edf2ee48523
Igor-balamuth <igor@balamuth.co.il>
2025-03-21 19:11:57 +0200

    volume control

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const audio=document.getElementById("player");
 const currentSongTitle=document.getElementById("currentSongTitle")
+const audioSource = document.getElementById("audioSource");
 const playlist=[
     {file:"aylex-winery.mp3", title:"aylex-winery"},
     {file: "fsm-team-happy-days.mp3", title:"fsm-team-happy-days" },
-    {file: "piki-to-the-glowing-forest.mp3", title:"piki-to-the-glowing-forest"}
+    {file: "piki-to-the-glowing-forest.mp3", title:"piki-to-the-glowing-forest"},
+    {file:"cinematic-epic-emotional-eglair.mp3", title:"cinematic-epic-emotional-eglair"},
+    {file:"mixaund-motivate-me.mp3",title:"mixaund-motivate-me"}
     
 ];
 let currentSongIndex=0;
@@ -32,6 +35,14 @@ function nextSong(){
     currentSongIndex=(currentSongIndex+1) % playlist.length;
     loadSong(currentSongIndex);
 }
+function prevSong(){
+    currentSongIndex=(currentSongIndex-1+playlist.length)%playlist.length
+        loadSong(currentSongIndex)
+}
+function setVolume(val){
+    audio.volume=val;
+}
+
 window.onload=function(){
     loadSong(0);
 }
